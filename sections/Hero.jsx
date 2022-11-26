b5abbb7969125edba5fd1f5b50e74df8ba0e2688
Adrien Pauchet <grandchefdotkong@gmail.com>
2022-11-26 17:55:27 +0900

    about & explore section updated

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => (
         <a href="#explore">
           <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
             <img
-              src="/stam.png" 
+              src="/stamp.png" 
               alt="stamp" 
               className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
             />
