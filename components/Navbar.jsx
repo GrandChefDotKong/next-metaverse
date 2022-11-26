655b3b5c00fa64801e7139b514ec5b6d59a75b51
Adrien Pauchet <grandchefdotkong@gmail.com>
2022-11-26 14:55:34 +0900

    hero section updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => (
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div className={`${styles.innerWidth} mx-auto flex justity-between gap-8`}>
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contain" />
       <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
         Meta_v3rs/Us
